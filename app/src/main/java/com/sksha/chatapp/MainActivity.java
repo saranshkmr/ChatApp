f55db7501ce21a111c0252573d6a1e88401098c0
saranshkmr <saranshkmr.in@gmail.com>
2016-07-27 21:07:58 +0530

    message class added

--- app/src/main/java/com/sksha/chatapp/MainActivity.java
+++ app/src/main/java/com/sksha/chatapp/MainActivity.java
@@ -3,11 +3,19 @@ package com.sksha.chatapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+
     }
 }
